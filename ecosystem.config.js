ee38d413d01dc7e0ec8abe0c51ce15ef4fa604a1
AdilsonBND <adilsonbnd@hotmail.com>
2022-07-29 22:08:24 -0300

    add pm

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "app",
+    script: ".src/api.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "prod",
+    },
+    env_production: {
+      NODE_ENV: "prod",
+    }
+  }]
+}
